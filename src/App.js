8e04f62c7d4b645b6033a0a6612d5fb81c65ce21
IgorCh76 <119522759+IgorCh76@users.noreply.github.com>
2023-02-27 20:53:57 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import RandomCocktail from "./components/RandomCocktail";
+
+function App() {
+  return (
+      <>
+        <RandomCocktail/>
+      </>
+  );
+}
+
+export default App;
